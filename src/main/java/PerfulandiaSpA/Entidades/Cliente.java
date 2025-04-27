8cf906ad393584eec4851c9cc9c1f6b74f6a47c2
Diego León <di.leonl@duocuc.cl>
2025-04-27 00:45:33 -0400

    Primer avance cliente/persona

---
+++ src/main/java/PerfulandiaSpA/Entidades/Cliente.java
@@ -0,0 +1,13 @@
+package PerfulandiaSpA.Entidades;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Cliente")
+public class Cliente extends Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+
+
+}
---
+++ src/main/java/PerfulandiaSpA/Entidades/Persona.java
@@ -0,0 +1,9 @@
+package PerfulandiaSpA.Entidades;
+
+public abstract class Persona {
+    protected int id;
+    protected String nombre;
+    protected String apellido;
+    protected String correo;
+    protected String telefono;
+}
