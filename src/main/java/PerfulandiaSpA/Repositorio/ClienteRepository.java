b1f83bfad8a851a61b8480412f6ffa127075f8ef
Diego León <di.leonl@duocuc.cl>
2025-05-10 22:06:44 -0400

    Test EsquemaBD

--- src/main/java/PerfulandiaSpA/Repositorio/ClienteRepository.java
+++ src/main/java/PerfulandiaSpA/Repositorio/ClienteRepository.java
@@ -1,9 +1,6 @@
 package PerfulandiaSpA.Repositorio;
-import PerfulandiaSpA.Entidades.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
