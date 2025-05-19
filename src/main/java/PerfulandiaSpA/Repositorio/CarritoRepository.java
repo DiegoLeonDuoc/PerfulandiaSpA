ead58992e31b6c111b41d32b15f64b0d0302da1d
Diego León <di.leonl@duocuc.cl>
2025-05-19 16:36:56 -0400

    Carrito
    Creación de Repo, servicio y Controller para clase Carrito.
    Funciones CRUD verificadas

---
+++ src/main/java/PerfulandiaSpA/Repositorio/CarritoRepository.java
@@ -0,0 +1,7 @@
+package PerfulandiaSpA.Repositorio;
+
+import PerfulandiaSpA.Entidades.Carrito;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarritoRepository extends JpaRepository<Carrito, Integer> {
+}
